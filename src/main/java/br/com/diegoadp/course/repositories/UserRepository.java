a6f83fee16890b34eb6569ab958cc09b5231cf94
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-04-05 11:02:49 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/br/com/diegoadp/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.diegoadp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.diegoadp.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
