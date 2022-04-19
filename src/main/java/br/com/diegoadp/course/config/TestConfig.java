c96eacd6c517cc5ff1fe0304a40d64cc5daef9b2
diegoadp <83314316+diegoadp@users.noreply.github.com>
2022-04-19 16:39:03 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/br/com/diegoadp/course/config/TestConfig.java
+++ src/main/java/br/com/diegoadp/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.diegoadp.course.entities.Category;
 import br.com.diegoadp.course.entities.Order;
+import br.com.diegoadp.course.entities.OrderItem;
 import br.com.diegoadp.course.entities.Product;
 import br.com.diegoadp.course.entities.User;
 import br.com.diegoadp.course.entities.enums.OrderStatus;
 import br.com.diegoadp.course.repositories.CategoryRepository;
+import br.com.diegoadp.course.repositories.OrderItemRepository;
 import br.com.diegoadp.course.repositories.OrderRepository;
 import br.com.diegoadp.course.repositories.ProductRepository;
 import br.com.diegoadp.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	ProductRepository productRepository;
+
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,6 +73,13 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 
 }
